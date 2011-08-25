9d05a6360a9d6e0842f57df6d5471f4f4981632b
luke <lfowlie890@gmail.com>
2011-08-25 02:26:49 -0400

    added imageview and options menu

--- src/com/sqrshare/android/AndroidSqrShareActivity.java
+++ src/com/sqrshare/android/AndroidSqrShareActivity.java
@@ -2,6 +2,8 @@ package com.sqrshare.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class AndroidSqrShareActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,12 @@ public class AndroidSqrShareActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
